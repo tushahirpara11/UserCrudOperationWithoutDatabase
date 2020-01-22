2ef3a1847e128a2b13635ff4887ed5fd35ec8740
tusharhirpara11 <tusharhirpara98@gmail.com>
2020-01-22 11:33:48 +0530

    add switchcase instead if-else in app.js

--- app.js
+++ app.js
@@ -6,40 +6,46 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
   let body = '';
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/JSON');
-  if (req.url === '/add' && req.method == 'POST') {
-    req.on('data', function (data) {
-      body += data;
-    });
-    req.on('end', function () {
-      res.end(userOperations.addUser(body));
-    });
-  }
-  if (req.url === '/delete' && req.method === 'DELETE') {
-    req.on('data', function (data) {
-      body += data;
-    });
-    req.on('end', function () {
-      res.end(userOperations.deleteUser(JSON.parse(body)));
-    });
-  }
-  else if (req.url === '/edit' && req.method === 'PUT') {
-    req.on('data', function (data) {
-      body += data;
-    });
-    req.on('end', function () {
-      res.end(userOperations.updateUser(JSON.parse(body)));
-    });
-  }
-  else if (req.url === '/read' && req.method === 'GET') {
-    req.on('data', function (data) {
-      body += data;
-    });
-    req.on('end', function () {
-      res.end(userOperations.getUser(JSON.parse(body)));
-    });
+  switch (req.url, req.method) {
+    case ('/add', 'POST'):
+      body = '';
+      req.on('data', function (data) {
+        body = body + data;
+      });
+      req.on('end', function () {
+        res.end(userOperations.addUser(body));
+      });
+      break;
+    case ('/delete', 'DELETE'):
+      body = '';
+      req.on('data', function (data) {
+        body = body + data;
+      });
+      req.on('end', function () {
+        res.end(userOperations.deleteUser(JSON.parse(body)));
+      });
+      break;
+    case ('/edit', 'PUT'):
+      body = '';
+      req.on('data', function (data) {
+        body = body + data;
+      });
+      req.on('end', function () {
+        res.end(userOperations.updateUser(JSON.parse(body)));
+      });
+      break;
+    case ('/read', 'GET'):
+      body = '';
+      req.on('data', function (data) {
+        body = body + data;
+      });
+      req.on('end', function () {
+        res.end(userOperations.getUser(JSON.parse(body)));
+      });
+      break;
   }
 }).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
